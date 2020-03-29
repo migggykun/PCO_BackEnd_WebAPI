f2301992512f0c8ed2e209e6e1fad7727d0e0f5b
migggykun <migggykun@gmail.com>
2020-03-29 13:36:05 +0400

    Added Null inserts to PRCDetails for registration

--- PCO_BackEnd_WebAPI/App_Start/MappingProfile.cs
+++ PCO_BackEnd_WebAPI/App_Start/MappingProfile.cs
@@ -20,36 +20,58 @@ namespace PCO_BackEnd_WebAPI.App_Start
     {
         public MappingProfile()
         {
+            //Accounts
+            Func<PRCDetail, ResponsePRCDetailDTO> convDTO = (x) => x == null ? new ResponsePRCDetailDTO() { Id = string.Empty, IdNumber = string.Empty, ExpirationDate = string.Empty } :
+                                                                             new ResponsePRCDetailDTO() { Id = x.Id.ToString(), IdNumber = x.IdNumber, ExpirationDate = x.ExpirationDate.Date.ToShortDateString() };
+            Func<RequestPRCDetailDTO, PRCDetail> conv = (x) =>
+                                                            {
+                                                                if (x != null && (string.IsNullOrEmpty(x.IdNumber) || string.IsNullOrEmpty(x.ExpirationDate)))
+                                                                {
+                                                                    return null;
+                                                                }
+                                                                else if (x != null && (!string.IsNullOrEmpty(x.IdNumber) || !string.IsNullOrEmpty(x.ExpirationDate)))
+                                                                {
+                                                                    return new PRCDetail()
+                                                                    {
+                                                                        IdNumber = x.IdNumber,
+                                                                        ExpirationDate = DateTime.Parse(x.ExpirationDate)
+
+                                                                    };
+                                                                }
+                                                                else //RequestPRCDetailDTO is null
+                                                                {
+                                                                    return null;
+                                                                }
+                                                            };
+
+
             Mapper.CreateMap<RequestAccountDTO, ApplicationUser>();
             Mapper.CreateMap<UserInfo, ResponseUserInfoDTO>();
             Mapper.CreateMap<RequestUserInfoDTO, UserInfo>();
             Mapper.CreateMap<MembershipType, ResponseMembershipTypeDTO>();
             Mapper.CreateMap<RequestMembershipTypeDTO, MembershipType>();
-
-            Func<PRCDetail, ResponsePRCDetailDTO> convDTO = (x) => x == null ? new ResponsePRCDetailDTO() { Id = string.Empty, IdNumber = string.Empty, ExpirationDate = string.Empty } :
-                                                                               new ResponsePRCDetailDTO() { Id = x.Id.ToString(), IdNumber = x.IdNumber, ExpirationDate = x.ExpirationDate.Date.ToShortDateString() };
-            Mapper.CreateMap<PRCDetail, ResponsePRCDetailDTO>().ConvertUsing(convDTO);
-
-            Func<RequestPRCDetailDTO, PRCDetail> conv = (x) => x.IdNumber == string.Empty ? null : new PRCDetail() { IdNumber = x.IdNumber, ExpirationDate = DateTime.Parse(x.ExpirationDate) };
             Mapper.CreateMap<RequestPRCDetailDTO, PRCDetail>().ConvertUsing(conv);
-                                                                        
+            Mapper.CreateMap<PRCDetail, ResponsePRCDetailDTO>().ConvertUsing(convDTO);
+                                                        
+            //Conference    
+            Mapper.CreateMap<Conference, ResponseConferenceDTO>().ForMember(dst => dst.PromoId, src => src.MapFrom(c => (c.PromoId == null) ? string.Empty : c.PromoId.Value.ToString()))
+                                                                 .ForMember(dst => dst.Banner, src => src.MapFrom(c => string.IsNullOrEmpty(c.Banner) ? string.Empty : c.Banner));
+            Mapper.CreateMap<RequestPromoDTO, Promo>().ForMember(dst => dst.PromoMembers, src => src.MapFrom(p => p.MembershipTypeIds.Select(mId => new PromoMember { MembershipTypeId = mId }).ToList())); ;
+            Mapper.CreateMap<Promo, ResponsePromoDTO>().ForMember(dst => dst.MembershipTypeIds, src => src.MapFrom(p => p.PromoMembers.Select(pm => pm.MembershipTypeId))); ;
             Mapper.CreateMap<AddConferenceDTO, Conference>();
             Mapper.CreateMap<UpdateConferenceDTO, Conference>();
-            Mapper.CreateMap<Conference, ResponseConferenceDTO>().ForMember(dst => dst.PromoId, src => src.MapFrom(c => (c.PromoId == null)?string.Empty : c.PromoId.Value.ToString()))
-                                                                 .ForMember(dst => dst.Banner, src => src.MapFrom(c => string.IsNullOrEmpty(c.Banner) ? string.Empty : c.Banner));
             Mapper.CreateMap<AddRateWithConferenceDTO, Rate>();
             Mapper.CreateMap<UpdateRateWithConferenceDTO, Rate>();
             Mapper.CreateMap<RequestRateDTO, Rate>();
             Mapper.CreateMap<Rate, ResponseRateDTO>();
-            Mapper.CreateMap<RequestPromoDTO, Promo>().ForMember(dst => dst.PromoMembers, src => src.MapFrom(p => p.MembershipTypeIds.Select(mId => new PromoMember { MembershipTypeId = mId}).ToList())); ;
-            Mapper.CreateMap<Promo, ResponsePromoDTO>().ForMember(dst => dst.MembershipTypeIds, src => src.MapFrom(p => p.PromoMembers.Select(pm => pm.MembershipTypeId)));;
             Mapper.CreateMap<RequestPromoMemberDTO, PromoMember>();
             Mapper.CreateMap<PromoMember, ResponsePromoMemberDTO>();
+
+            //Registration
             Mapper.CreateMap<RequestRegistrationDTO, Registration>().ForMember(dst => dst.RegistrationStatusId, x => x.MapFrom(a => 1));
             Mapper.CreateMap<Registration, ResponseRegistrationDTO>();
             Mapper.CreateMap<Registration, ResponseListRegistrationDTO>();
             Mapper.CreateMap<ApplicationUser, ResponseAccountDTO>();
-
             Mapper.CreateMap<AddPaymentDTO, Payment>();
             Mapper.CreateMap<UpdatePaymentDTO, Payment>();
             Mapper.CreateMap<Payment, ResponsePaymentDTO>();
