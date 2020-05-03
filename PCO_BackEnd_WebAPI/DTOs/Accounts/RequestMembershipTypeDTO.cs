9e7a71909a05cbecb3f37b52ba58172cf75da04d
Erevisiae <59959744+Erevisiae@users.noreply.github.com>
2020-05-03 15:35:36 +0800

    Update MembershipType DTO
    1. Fix error in MembershipType by adjusting max lengths

--- PCO_BackEnd_WebAPI/DTOs/Accounts/RequestMembershipTypeDTO.cs
+++ PCO_BackEnd_WebAPI/DTOs/Accounts/RequestMembershipTypeDTO.cs
@@ -9,11 +9,11 @@ namespace PCO_BackEnd_WebAPI.DTOs.Accounts
     public class RequestMembershipTypeDTO
     {
         [Required]
-        [StringLength(128, ErrorMessage = "{0} length must be lesser than {1}.")]
+        [StringLength(128, ErrorMessage = "{0} length must be between 1 and {1}!")]
         public string Name { get; set; }
 
         [Required]
-        [StringLength(512, ErrorMessage = "{0} length must be between {2} and {1}.", MinimumLength = 2)]
+        [StringLength(512, ErrorMessage = "{0} length must be between 1 and {1}!")]
         public string Description { get; set; }
     }
 }
