5a68f8cbef624df72ca1202054ab9a9e39ccb9a9
jonasmarte <59923143+jonasmarte@users.noreply.github.com>
2020-12-03 05:45:45 +0800

    Add to last commit, update member type
    Add to last commit, update member type

--- PCO_BackEnd_WebAPI/Controllers/Accounts/MemberController.cs
+++ PCO_BackEnd_WebAPI/Controllers/Accounts/MemberController.cs
@@ -99,6 +99,7 @@ namespace PCO_BackEnd_WebAPI.Controllers.Accounts
                 ApplicationUser updateMembership = await Task.Run(() => unitOfWork.Accounts.UserManager.FindByIdAsync(userId));
                 updateMembership.IsActive = true;
                 updateMembership.IsMember = true;
+                updateMembership.UserInfo.MembershipTypeId = 1; //default value for associate Member
                 await Task.Run(()=>unitOfWork.Accounts.UpdateAccount(userId, updateMembership));
                 
                 await Task.Run(() => unitOfWork.Complete());
@@ -145,6 +146,7 @@ namespace PCO_BackEnd_WebAPI.Controllers.Accounts
 
                     ApplicationUser updateMembership = await Task.Run(() => unitOfWork.Accounts.UserManager.FindByIdAsync(userId));
                     updateMembership.IsActive = member.IsActive;
+                    updateMembership.UserInfo.MembershipTypeId = member.IsActive?1:0; //default value for associate Member (1) Non-member (0)
                     await Task.Run(() => unitOfWork.Accounts.UpdateAccount(userId, updateMembership));
 
                     await Task.Run(() => unitOfWork.Complete());
@@ -183,6 +185,7 @@ namespace PCO_BackEnd_WebAPI.Controllers.Accounts
                     ApplicationUser updateMembership = await Task.Run(() => unitOfWork.Accounts.UserManager.FindByIdAsync(userId));
                     updateMembership.IsActive = false;
                     updateMembership.IsMember = false;
+                    updateMembership.UserInfo.MembershipTypeId = 0;
                     await Task.Run(() => unitOfWork.Accounts.UpdateAccount(userId, updateMembership));
 
                     await Task.Run(() => unitOfWork.Complete());
